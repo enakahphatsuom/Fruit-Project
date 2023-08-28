f44d2d254a7bd97ff1b7bee25ddc5203a1c68a38
enakahphatsuom <moustaphaekane302@gmail.com>
2023-08-28 14:07:17 -0500

    first commit

---
+++ autocomplete_starter_code/starter_code/script.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+let searchString = '';
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+
+	results = fruit.filter(item => item.toLowerCase().includes(str));
+
+	return results;
+}
+
+function searchHandler(e) {
+	searchString = input.value.trim().toLowerCase();
+	let results = search(searchString);
+	showSuggestions(results, searchString);
+}
+
+function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = '';
+
+	results.forEach(element => {
+		const suggestionItem = document.createElement('li');
+		suggestionItem.textContent = element;
+		suggestions.appendChild(suggestionItem);
+	});
+}
+
+function useSuggestion(e) {
+	input.value = e.target.textContent;
+	suggestions.style.display = 'none';
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
